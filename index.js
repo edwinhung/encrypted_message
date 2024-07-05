1af57095809cf70c015a2d1f45328335ecfb52ff
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-04 18:38:54 -0700

    generate a link with secrete message

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
